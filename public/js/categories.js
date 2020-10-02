fafadb6a47d0f01af56737f1890a6206a95419aa
Jason Tilley <jttilley007@yahoo.com>
2020-10-01 20:43:34 -0600

    Removed initialize route and made categories variable

---
+++ public/js/categories.js
@@ -0,0 +1,134 @@
+const categories = [
+  {
+    name: "Income",
+    subCategories: [
+      "Weekly", 
+      "Bi-Weekly", 
+      "Monthly", 
+      "One Time"
+    ]
+  },{
+    name: "Mortgage/Rent",
+    startPercent: 25,
+    subCategories: [
+      "1st Mortgage",
+      "2nd Mortgage",
+      "Rent",
+      "Repairs/Maint.",
+      "Real Estate Taxes",
+      "Association Dues"
+    ]
+  },{
+    name: "Utilities",
+    startPercent: 5,
+    subCategories: [
+      "Electricity",
+      "Gas",
+      "Water",
+      "Trash",
+      "Phone(s)",
+      "Internet",
+      "TV Package",
+      "Sling",
+      "Netflix",
+      "Amazon Prime",
+      "Hulu",
+      "Disney"
+    ]
+  },{
+    name: "Insurance",
+    startPercent: 5,
+    subCategories: [
+      "Life",
+      "Health",
+      "Car",
+      "Home Owners/Renters",
+      "Disability",
+      "Long-Term Care"
+    ]
+  },{
+    name: "Giving",
+    startPercent: 5,
+    subCategories: [
+      "Tithes",
+      "Charity"
+    ]
+  },{
+    name: "Transportation",
+    startPercent: 5,
+    subCategories: [
+      "Gas & Oil",
+      "Repairs & Tires",
+      "License & Taxes",
+      "Bus/Train Passes"
+    ]
+  },{
+    name: "Debt",
+    startPercent: 5,
+    subCategories: [
+      "Credit Card",
+      "Car Payment",
+      "Student Loan"
+    ]
+  },{
+    name: "Food",
+    startPercent: 5,
+    subCategories: [
+      "Groceries",
+      "Resturants/Takeout"
+    ]
+  },{
+    name: "Medical/Health",
+    startPercent: 5,
+    subCategories: [
+      "Medications",
+      "Doctors Bills",
+      "Dentist",
+      "Optometrist",
+      "Vitamins"
+    ]
+  },{
+    name: "Personal",
+    startPercent: 5,
+    subCategories: [
+      "Child Care/Sitter",
+      "Toiletries",
+      "Cosmetics/Hair Care",
+      "Education/Tuition",
+      "Books/Supplies",
+      "Child Support",
+      "Alimony",
+      "Subscriptions",
+      "Organization Dues",
+      "Pocket Money",
+      "Baby Supplies",
+      "Pet Supplies",
+      "Music/Technology",
+      "Misculaneous"
+    ]
+  },{
+    name: "Recreation",
+    startPercent: 5,
+    subCategories: [
+      "Gym Membership",
+      "Entertainment",
+      "Trainer",
+      "Sport Dues"
+    ]
+  },{
+    name: "Savings",
+    startPercent: 5,
+    subCategories: [
+      "Vacation",
+      "Christmas",
+      "Birthdays",
+      "Retirement",
+      "Emergency Fund"
+    ]
+  },{
+    name: "Clothing",
+    startPercent: 5,
+    subCategories: ["Adults", "Children", "Cleaning/Laundry"]
+  }
+];
+  module.exports = categories
