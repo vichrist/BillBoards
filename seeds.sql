fc9c3248e7553e313d0d86fd3b06f9d178f489a0
Christal Vigil <c.vigil27@gmail.com>
2020-10-03 19:52:26 -0600

    created seeds.sql for testing and fixed error on API get call

---
+++ seeds.sql
@@ -0,0 +1,10 @@
+USE budget_db; 
+
+INSERT INTO budgets (business, budgetName) VALUES (true, 'test');
+
+
+INSERT INTO budgetentries (business, budgetExpenses, amount, name, category) VALUES (true, true, 40.0, 'TEST', 'TEST');
+
+
+INSERT INTO categories (business, categoryName, subcategoryName VALUES (true,'TEST', 'TEST');
+
