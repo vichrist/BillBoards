10d5964d822b50c9ab733ca13251612676325e02
Christal Vigil <c.vigil27@gmail.com>
2020-10-04 17:02:21 -0600

    updating

--- seeds.sql
+++ seeds.sql
@@ -1,3 +1,4 @@
+
 USE budget_db; 
 
 INSERT INTO users (email, password) VALUES ('test@gmail.com', 'password'); 
@@ -7,3 +8,5 @@ INSERT INTO budgets (business, budgetName, userId) VALUES (true, 'test', 1);
 INSERT INTO budgetentries (business, budgetExpenses, amount, name, category, budgetId) VALUES (true, true, 40.0, 'TEST', 'TEST', 1);
 
 INSERT INTO categories (business, categoryName, subcategoryName, userId) VALUES (true,'TEST', 'TEST', 1);
+
+
