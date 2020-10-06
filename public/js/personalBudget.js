9ea1f609d97b251a3f2c2e053499030d622f626f
haaleylew <66989529+haaleylew@users.noreply.github.com>
2020-10-05 19:18:12 -0600

    sorry oop

---
+++ public/js/personalBudget.js
@@ -0,0 +1,9 @@
+
+$('.subcategory-row').hide();
+$('.category-row').click(function (event) {
+    var subcategory_class, category_id;
+    category_id = $(this).attr('id');
+    subcategory_identifier = '.' + category_id + '-subcategory';
+    $(subcategory_identifier).toggle();
+});
+
